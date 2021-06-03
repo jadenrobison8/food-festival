455cb2c4ab78c8fc9712e2969f50340fa9028811
robisonja <jadenrobison8@gmail.com>
2021-06-03 17:33:14 -0600

    add offline functionality

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
    },
    output: {
       path: __dirname + "/dist",
-      filename: '[name].bundle.js'
+      filename: '[name].bundle.js',
    },
    module: {
       rules: [
@@ -34,7 +34,7 @@ module.exports = {
                   loader: 'image-webpack-loader'
                }
             ]
-         }
+         },
       ]
    },
    plugins: [
